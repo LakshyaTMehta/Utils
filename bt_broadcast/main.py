5484c5eeafefb31b45a135b0fb49acbe92f7dcfb
Lakshya T Mehta <131202380+LakshyaTMehta@users.noreply.github.com>
2024-10-27 05:46:31 +0530

    Update main.py
    Removed unnecessary comments

--- bt_broadcast/main.py
+++ bt_broadcast/main.py
@@ -1,32 +1,6 @@
 from subprocess import check_output, CalledProcessError, run
 
 
-"""
-Sink #3
-        State: SUSPENDED
-        Name: combination-sink
-        Description: Simultaneous output to Pebble Comet, PEBBLE XS
-        Driver: module-combine-sink.c
-        Sample Specification: s16le 2ch 44100Hz
-        Channel Map: front-left,front-right
-        Owner Module: 25
-        Mute: no
-        Volume: front-left: 65536 / 100% / 0.00 dB,   front-right: 65536 / 100% / 0.00 dB
-                balance 0.00
-        Base Volume: 65536 / 100% / 0.00 dB
-        Monitor Source: combination-sink.monitor
-        Latency: 0 usec, configured 0 usec
-        Flags: DECIBEL_VOLUME LATENCY
-        Properties:
-                device.class = "filter"
-                slaves = "bluez_sink.18_0E_E9_A7_87_01.a2dp_sink,bluez_sink.4B_86_2E_28_96_0C.a2dp_sink"
-                device.description = "Simultaneous output to Pebble Comet, PEBBLE XS"
-                device.icon_name = "audio-card"
-        Formats:
-                pcm
-"""
-
-
 def main():
     try:
         check_output(['pulseaudio', '--version'], text=True)
@@ -52,7 +26,6 @@ def main():
 
     # setting the combined sink as default
     run(['pactl', 'set-default-sink', 'bt_broadcast_sink'])
-
 
 
 if __name__ == '__main__':
